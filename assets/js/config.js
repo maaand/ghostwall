fcba01e251b2496edb0da191feef050cb164c4fa
Meaglith Ma <genedna@gmail.com>
2014-01-09 11:09:35 +0800

    Init with ghostwall right side theme

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+/*================================================================*/
+/*  1. Global Variables Define
+/*================================================================*/
+var flickr_id='YOUR_FLICKR_ID_HERE';
+var instagram_accessToken='your-instagram-access-token'; // change [your-instagram-access-token] here
+var instagram_clientID='your-instagram-application-clientID'; // change [your-instagram-application-clientID] here
+var your_latitude   = 10.839776; // Your map latitude
+var your_longitude  = 106.647159; // Your map longitude
+var enable_tag_count= false;
+var rememberLastActiveTag=false;
